d5f59fa9e72516c7dce8debf7e6d872eea9c332c
Abiola Fahad Ogunnaike <realabiolafahad@gmail.com>
2025-04-25 11:31:40 +0100

    Add user route and user view template

---
+++ Laravel Projects/demo-project/resources/views/user.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>User Page</title>
+</head>
+<body>
+    <h1>User Page</h1>
+    <p>Marhaban Ya Abu Fulaan!</p>
+    <p>Your email is: abufulaan@mail.com</p>
+
+    <h2>Posts:</h2>
+    <p>Nothing posted yet</p>
+
+    <a href="/">Back to Home</a>
+</body>
+</html>
